5d07715513c1acd4c1d9f2f69d5e672806f24eed
adrianvrj <adrivargas48@gmail.com>
2023-03-11 15:40:52 -0600

    configuracion basica del server de express

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'ejs');
+//utilizar .ejs para las views en el folder de views
+app.set('views', path.join(__dirname, 'views'));
+
+//json parser para las consultas a bd
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//carpeta public para imgs y demas
+app.use(express.static('public'));
+app.use(express.static('files'));
+
+
+app.get('/', function(req, res){
+    res.render("index");
+})
+
+app.listen('9000', () =>{
+    console.log('Server running on port 9000');
+});
+
