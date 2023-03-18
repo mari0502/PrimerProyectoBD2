b457250cb112ebbb7b06d8b9a4b2c68382a39e92
Mariana <mfernandezmar3@gmail.com>
2023-03-18 11:12:49 -0600

    parte inicial del login

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const mongoose = require('./database_connections/mongo');
 const neo4jSession = require('./database_connections/neo4j');
 const redis = require('./database_connections/redis');
-const mysqlconn = require('./database_connections/mysql');
+//const mysqlconn = require('./database_connections/mysql');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -15,9 +15,11 @@ app.set('views', path.join(__dirname, 'views'));
 //json parser para las consultas a bd
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
 //carpeta public para imgs y demas
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(express.static('files'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res){
     res.render("index");
